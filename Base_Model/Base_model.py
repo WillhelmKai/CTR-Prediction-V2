2a43e050d9e479dfe5f6e268bfae624ea3150bca
WillhelmKai <Willhelmkai@outlook.com>
2019-03-09 13:41:48 +0800

    Update Base_model.py

--- Base_Model/Base_model.py
+++ Base_Model/Base_model.py
@@ -7,15 +7,39 @@ os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
 trainning_set_ad ="C:\\Users\\willh\\Documents\\FYP2\\DataLundary\\RecordsTextOnly\\back\\TrainingSet.json"
 # ————————————————————————————
-#target record strcuture: ID192403  review1689188
-#[reviewerID[1],behavior[multi-dimension, numpy array stored data Frame](asin, brand, categories, unixReviewTime, price, overall),
+#record strcuture: ID192403  review1689188
+#[behavior[multi-dimension, numpy array stored data Frame](asin, brand, categories, unixReviewTime, price, overall),
 #CandidateAd[1](asin, brand, categories, unixReviewTime, price), label[1] ]
 # ————————————————————————————
 
 # ————————————————————————————
-#Embedding Layer
+#Embedding Layer start
 # ————————————————————————————
 
 # ————————————————————————————
+#Embedding Layer end
 # ————————————————————————————
 
+# ————————————————————————————
+#interest extractor layer start
+# ————————————————————————————
+
+# ————————————————————————————
+#interest extractor layer end
+# ————————————————————————————
+
+# ————————————————————————————
+#interest evolving layer start
+# ————————————————————————————
+
+# ————————————————————————————
+#interest evolving layer end
+# ————————————————————————————
+
+# ————————————————————————————
+#NN
+# ————————————————————————————
+
+# ————————————————————————————
+#NN
+# ————————————————————————————
