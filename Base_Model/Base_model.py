f2fa60466d7bf3097666dcacd6e6645064cb6085
WillhelmKai <Willhelmkai@outlook.com>
2019-03-09 14:14:17 +0800

    Update Base_model.py

--- Base_Model/Base_model.py
+++ Base_Model/Base_model.py
@@ -1,16 +1,30 @@
 #coding by Willhelm
 #20190309
-import tensorflow as tf
+import pandas as pd
 import numpy as np
+import os
+# import tensorflow as tf
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
-trainning_set_ad ="C:\\Users\\willh\\Documents\\FYP2\\DataLundary\\RecordsTextOnly\\back\\TrainingSet.json"
+json_add ="C:\\Users\\willh\\Documents\\FYP2\\DataLundary\\RecordsTextOnly\\back\\TrainingSet.json"
 # ————————————————————————————
 #record strcuture: ID192403  review1689188
 #[behavior[multi-dimension, numpy array stored data Frame](asin, brand, categories, unixReviewTime, price, overall),
 #CandidateAd[1](asin, brand, categories, unixReviewTime, price), label[1] ]
 # ————————————————————————————
+# ————————————————————————————
+#input
+# ————————————————————————————
+def read_records(add):
+    training_set = 0
+    testing_set = 0
+    dr= pd.read_json(open(add).read(), lines=True)
+    dr = pd.DataFrame.as_matrix(dr)
+    print(dr.shape())
+    return training_set, testing_set
+
+read_records(json_add)
 
 # ————————————————————————————
 #Embedding Layer start
@@ -37,9 +51,18 @@ trainning_set_ad ="C:\\Users\\willh\\Documents\\FYP2\\DataLundary\\RecordsTextOn
 # ————————————————————————————
 
 # ————————————————————————————
-#NN
+#NN start
 # ————————————————————————————
 
 # ————————————————————————————
-#NN
+#NN end
 # ————————————————————————————
+
+# ————————————————————————————
+#training start
+# ————————————————————————————
+
+# ————————————————————————————
+#Evaluation start
+# ————————————————————————————
+
