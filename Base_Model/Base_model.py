f6cf1c5a6db64c4f275c98fbac6c5aa82db777d7
WillhelmKai <Willhelmkai@outlook.com>
2019-03-10 22:42:34 +0800

    Update Base_model.py

--- Base_Model/Base_model.py
+++ Base_Model/Base_model.py
@@ -1,13 +1,15 @@
 #coding by Willhelm
 #20190309
+# import tensorflow as tf
 import pandas as pd
 import numpy as np
 import os
-# import tensorflow as tf
 
 #os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
-json_add ="C:\\Users\\willh\\Documents\\FYP2\\DataLundary\\RecordsTextOnly\\back\\TrainingSet.json"
+# json_add ="C:\\Users\\willh\\Documents\\FYP2\\DataLundary\\RecordsTextOnly\\back\\TrainingSet.json"
+json_add = '/home/ubuntu/fyp2/LundaryBack/TrainingSet.json'
+
 # ————————————————————————————
 #record strcuture: ID192403  review1689188
 #[behavior[multi-dimension, numpy array stored data Frame](asin, brand, categories, unixReviewTime, price, overall),
@@ -24,8 +26,7 @@ behavior = [d['behavior'] for d in l]
 candidateAd = [d['candidateAd'] for d in l]
 label = [d['label'] for d in l]
 
-
-#reviewerID, asin, categories, price, brand, overall, unixReviewTime
+#reviewerID, asin, categories[3526], price, brand[738], overall, unixReviewTime
 
 for behavior_for_one_comstomer in behavior:
     for one_behavior in behavior_for_one_comstomer:
@@ -34,8 +35,10 @@ for behavior_for_one_comstomer in behavior:
         price = one_behavior[5]
         review_time = one_behavior[7]
         print(cate)
+        print(len(cate))
         print("   ")
         print(brand)
+        print(len(brand))
         print("   ")
         print(price)
         print("   ")
@@ -43,13 +46,21 @@ for behavior_for_one_comstomer in behavior:
         print("   ")
         break
     break
+
 # ————————————————————————————
 #Embedding Layer start
 # ————————————————————————————
+# categories_input = tf.placeholder(tf.float32, [None, 100])
+# brand_input = tf.placeholder(tf.float32, [None, 100])
+# price_input = tf.placeholder(tf.float32, [None, 100])
+# review_time_input = tf.placeholder(tf.float32, [None, 100])
 
 # ————————————————————————————
 #Embedding Layer end
 # ————————————————————————————
+
+
+
 
 # ————————————————————————————
 #interest extractor layer start
