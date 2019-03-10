24f989b91404006b6fb9f726a923a11d150027c2
WillhelmKai <Willhelmkai@outlook.com>
2019-03-11 00:00:28 +0800

    Update FormateTrainingSet.py

--- Data_Lundary/FormateTrainingSet.py
+++ Data_Lundary/FormateTrainingSet.py
@@ -4,7 +4,7 @@ import json
 import gzip
 import pandas as pd
 import numpy as np
-import tensorflow
+import tensorflow as tf
 from sklearn.preprocessing import MultiLabelBinarizer 
 pd.set_option('display.max_columns', 10000, 'display.max_rows', 10000)
 
@@ -25,25 +25,21 @@ def generate_false_candidate(behavior):
             break
     return result 
 
-def parse(path):
-  g = gzip.open(path, 'rb')
-  for l in g:
-    yield eval(l)
-
-def getDF(path):
-  i = 0
-  df = {}
-  for d in parse(path):
-    df[i] = d
-    i += 1
-  return pd.DataFrame.from_dict(df, orient='index')
+def convert_to_numpy(input):
+    result = [] 
+    for item in input:
+        result.append(item)
+    result = np.array(result)
+    return result
 # ————————————————————————————
 #target record strcuture: ID192403  review1689188
 #[reviewerID[1],behavior[multi-dimension, numpy array stored data Frame](asin, brand, categories, unixReviewTime, price, overall),
 #CandidateAd[1](asin, brand, categories, unixReviewTime, price), label[1] ]
 # ————————————————————————————
 #destination
-traning_set_add = 'C:\\Users\\willh\\Documents\\FYP2\\DataLundary\\RecordsTextOnly\\TrainingSet.json'
+# traning_set_add = 'C:\\Users\\willh\\Documents\\FYP2\\DataLundary\\RecordsTextOnly\\TrainingSet.json'
+tfrecord = 'C:\\Users\\willh\\Documents\\FYP2\\DataLundary\\RecordsTextOnly\\TrainingSet.tfrecords'
+
 text_json_add = 'C:\\Users\\willh\\Documents\\FYP2\\DataLundary\\RecordsTextOnly\\StrcuturedTextOnly.json'
 
 # traning_set_add = '/home/ubuntu/fyp2/LundaryBack/TrainingSet.json'
@@ -53,7 +49,7 @@ data_str = open(text_json_add).read()
 df = pd.read_json(data_str, lines= True)
 # ————————————————————————————
 # ————————————————————————————
-df = df[0:int(len(df)*0.001)]
+df = df[0:int(len(df)*0.01)]
 
 # ————————————————————————————
 # ————————————————————————————
@@ -84,14 +80,15 @@ print("finished")
 # select columns and remove ruplicate
 print("forming reviewer dictionary")
 reviewID_dic = df[['reviewerID']].drop_duplicates().reset_index(drop=True)
-print("re: "+str(reviewID_dic.columns ))
+# print("re: "+str(reviewID_dic.columns ))
 
 # ————————————————————————————
 #make up training set
 # ————————————————————————————
-lower_boundary = 5  #if the num of user behavior lower than this num, record will be abandoned 
+lower_boundary = 3  #if the num of user behavior lower than this num, record will be abandoned 
 acc = 0
 df_result = [] #final datastructure to store 
+writer = tf.python_io.TFRecordWriter(tfrecord)
 for reviewerID in reviewID_dic.itertuples(index = False):
     #search all his or her historical behaviors sorted by time
     his_behavior = df.loc[df['reviewerID'] == reviewerID[0]].sort_values(by = ['unixReviewTime']).reset_index(drop=True)
@@ -99,31 +96,45 @@ for reviewerID in reviewID_dic.itertuples(index = False):
     if (len(his_behavior) >= lower_boundary):
         # his_behavior.ix[0,len(his_behavior),['categories']] = his_behavior.ix[0,len(his_behavior),['categories']][0][0]
         for i in range(lower_boundary ,len(his_behavior)): #select the behavior sequence number as the candidata ad
-            behavior = his_behavior.loc[0:i-1] #historical behavior
-            behavior = behavior.to_records()
+            behavior = his_behavior.loc[0:i-1]['categories'] #historical behavior
+            behavior_categories = convert_to_numpy(behavior)
+            print("   shape of behavior ")
+            print(behavior_categories.shape)
+            print(behavior_categories)
+            # behavior = behavior.to_records()
+            print("  ")
+            candidate_true = his_behavior.loc[i]['categories'] #candidate ad
+            print(candidate_true)
 
-            candidate_true = his_behavior.loc[i] #candidate ad
-            candidate_false = generate_false_candidate(his_behavior.loc[0:i-1])
+            example = tf.train.Example(features = tf.train.Features(feature = {
+            'candidate_categories':_float_feature(candidate_true),
+            'behavior_categories':_float_feature(behavior_categories)
+            }))
+            # candidate_false = generate_false_candidate(his_behavior.loc[0:i-1])
+            # # #generate true record
+            # t = {'behavior':behavior,'candidateAd':candidate_true, 'label' : 1 }
+            # df_result.append(t)
+            # #generate false record
+            # d = {'behavior':behavior,'candidateAd':candidate_false, 'label' : 0 }
+            # df_result.append(d)
+            writer.write(example.SerializeToString())
 
-            # #generate true record
-            t = {'behavior':behavior,'candidateAd':candidate_true, 'label' : 1 }
-            df_result.append(t)
-            #generate false record
-            d = {'behavior':behavior,'candidateAd':candidate_false, 'label' : 0 }
-            df_result.append(d)
+
     else:#skip customers who have insufficient bahavior
         pass
     acc = acc+1
+    break
     if (acc%5000):
         print('\r', str(acc/len(reviewID_dic)).ljust(10),end='')
 df_result = pd.DataFrame.from_records(np.asarray(df_result))
-print(df_result.head())
-out = df_result.to_json(orient='records')
+writer.close()
 # test = pd.read_json(out, lines= True)
 # print("---------------")
 # print(test.head().reset_index(drop=True))
-with open(traning_set_add, 'a') as f:
-    f.write(out)
+
+# with open(traning_set_add, 'a') as f:
+#     f.write(out)
+
 # ————————————————————————————
 # the end
 # ————————————————————————————
