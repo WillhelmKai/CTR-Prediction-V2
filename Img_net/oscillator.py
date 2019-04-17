17163c3337cc2243000ecf35ced3233541d2bf02
WillhelmKai <Willhelmkai@outlook.com>
2019-04-17 12:13:30 +0800

    Update oscillator.py

--- Img_net/oscillator.py
+++ Img_net/oscillator.py
@@ -6,25 +6,26 @@ import os
 import matplotlib.pyplot as plt
 
 def oscillator(I, u, v, z):
-    a1, a2, a3, a4 = 0.6, 0.6, -0.5, 0.5
-    b1, b2, b3, b4 = -0.6, -0.6, -0.5, 0.5
+    a1, a2, a3, a4 = -0.5, 0.55, 0.55, 0.5
+    b1, b2, b3, b4 = -0.5, -0.55, -0.55, 0.5
     k = 50
 
-    u_v = np.tanh(a1*u - a2*v + a3*z + a4*I)
-    v_v = np.tanh(b3*z - b1*u - b2*v+b4*I)
-    w = np.tanh(I)
-    z_v = ( v_v - u_v )* np.exp(-k * (I**2))+w
+    u_v = math.tanh(a1*u + a2*v - a3*z + a4*I)
+    v_v = math.tanh(b1*z - b2*u - b3*v + b4*I)
+    w = math.tanh(I)
+    z_v = ( v_v - u_v )* math.exp(-k*I*I)+ w
     return z_v, u_v, v_v
 
-u, v, z= 0, 0, 0
-x = np.arange(0,100)
-x = x/100
-y = []
-for i in range(0,len(x)):
-    z_temp,u_temp,v_temp= oscillator(x[i], u, v, z)
-    u,v,z = u_temp,v_temp,z_temp
-    y.append(z)
-y = np.array(y)
 
-plt.plot(x,y)
+x = np.arange(-100,100)
+x = x/100
+N = 100
+# for every single element
+for i in range(0,len(x)):
+    #each element in the time sequence T
+    print('\r', "progress ",str(i/len(x)).ljust(10),end='')
+    u, v, z = np.zeros(N),np.zeros(N),np.zeros(N)
+    for time in range(0,N-1):
+        z[time+1], u[time+1],v[time+1] = oscillator(x[i], u[time], v[time], z[time])
+        plt.plot(x[i], z[time+1], 'c,')
 plt.show()
