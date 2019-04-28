fe076207dc9cd66a51fb206bc8760413e5d8b54b
WillhelmKai <Willhelmkai@outlook.com>
2019-04-28 16:33:31 +0800

    Update img_net.py

--- Img_net/img_net.py
+++ Img_net/img_net.py
@@ -28,9 +28,9 @@ def read_img(name):
         result = cv2.resize(cv2.imread(name),(112,112), interpolation=cv2.INTER_CUBIC)
 
     except Exception as e:
-        name = img_add+'0594514681.jpg'
-        result = cv2.resize(cv2.imread(name),(112,112), interpolation=cv2.INTER_CUBIC)
         print("empty add")
+        name = img_add+'0594033934.jpg'
+        result = cv2.resize(cv2.imread(name),(112,112), interpolation=cv2.INTER_CUBIC)
 
     result = cv2.cvtColor(result, cv2.COLOR_BGR2RGB)
     result = result.astype(np.float32)
@@ -43,26 +43,27 @@ def behavior_img(behavior_asin):
     for i in range(0,len(behavior_asin)):
         #filte the unrelated info 
         if(i%2):
+            name = img_add+behavior_asin[i]+'.jpg'
             try:
-                name = img_add+behavior_asin[i]+'.jpg'
                 temp = cv2.resize(cv2.imread(name),(112,112), interpolation=cv2.INTER_CUBIC)
                 result.append(temp)
             except Exception as e:
-                name = img_add+'0594514681.jpg'
+                print("empty add")
+                name = img_add+'0594033934.jpg'
+                print(name)
                 temp = cv2.resize(cv2.imread(name),(112,112), interpolation=cv2.INTER_CUBIC)
                 result.append(temp)
-                print("empty add")
     result = np.array(result)
     # print(result.shape())
     return result
 
-training_set = 'C:\\Users\\willh\\Documents\\FYP2\\DataLundary\\RecordsTextOnly\\TrainingSet.tfrecords'
-testing_set = 'C:\\Users\\willh\\Documents\\FYP2\\DataLundary\\RecordsTextOnly\\TestingSet.tfrecords'
-img_add = 'D:\\Y4\\FYP2\\amazon_raw_data\\img_unziped\\Amazon_img\\'
+# training_set = 'C:\\Users\\willh\\Documents\\FYP2\\DataLundary\\RecordsTextOnly\\TrainingSet.tfrecords'
+# testing_set = 'C:\\Users\\willh\\Documents\\FYP2\\DataLundary\\RecordsTextOnly\\TestingSet.tfrecords'
+# img_add = 'D:\\Y4\\FYP2\\amazon_raw_data\\img_unziped\\Amazon_img\\'
 
-
-# training_set = '/home/ubuntu/fyp2/LundaryBack/TrainingSet.tfrecords'
-# testing_set= '/home/ubuntu/fyp2/LundaryBack/TestingSet.tfrecords'
+training_set = '/home/ubuntu/fyp2/LundaryBack/TrainingSet.tfrecords'
+testing_set= '/home/ubuntu/fyp2/LundaryBack/TestingSet.tfrecords'
+img_add = '/home/ubuntu/fyp2/LundaryBack/Amazon_img/'
 # ———————————————————————————— 
 #total 192403 records
 #categories (1,738), brand (1,3526)
@@ -72,9 +73,9 @@ img_add = 'D:\\Y4\\FYP2\\amazon_raw_data\\img_unziped\\Amazon_img\\'
 # ————————————————————————————
 #training set
 
-epoch = 1 #25 
-iteration = 5 #307844
-iteration_test = 100#60658
+epoch = 3 #25 
+iteration = 192403
+iteration_test = 60658
 reader = tf.TFRecordReader()
 train_queue = tf.train.string_input_producer([training_set], num_epochs=None)
 _, serialized_example = reader.read(train_queue)
@@ -231,12 +232,17 @@ W_crt=weight_variable([1, 50]) #out [-1, 50]
 b_crt=bias_variable([50])
 embeded_ctr = tf.nn.tanh(tf.matmul(ph_candidate_review_time, W_crt)+b_crt)
 
-embedding_candidate_out = tf.concat([embeded_cc,embeded_cb,embeded_cp,embeded_ctr], 1)#out  [-1,1600]
+with tf.name_scope('embedding_candidate_out'):
+    embedding_candidate_out = tf.concat([embeded_cc,embeded_cb,embeded_cp,embeded_ctr], 1)#out  [-1,1600]
+    tf.summary.histogram('embedding_candidate_out', embedding_candidate_out)
 
 
 #attention machanism out[-1,1100]
 W_attention = weight_variable([1100,1100])
 attention_intermidiate_output = tf.matmul(first_GRU_outputs, tf.matmul(W_attention, tf.transpose(embedding_candidate_out)))
+#@@@@@@@@@@@@@@@@@@@
+attention_intermidiate_output = tf.nn.tanh(attention_intermidiate_output)
+#@@@@@@@@@@@@@@@@@@@
 attention_output = tf.div(tf.exp(attention_intermidiate_output),tf.reduce_sum(tf.exp(attention_intermidiate_output)))
 
 #second GRU
@@ -252,9 +258,14 @@ second_GRU_outputs, final_state_second = tf.nn.dynamic_rnn(cell, second_GRU_inpu
 # ————————————————————————————
 #read the candidate img from the directory
 # ————————————————————————————
-embeded_ca = conver.converlutional(ph_candidate_asign_img) #out [-1,500] candidate img
+with tf.name_scope('embeded_ca'):
+    embeded_ca = conver.converlutional(ph_candidate_asign_img) #out [-1,500] candidate img
+    tf.summary.histogram('embeded_ca', embeded_ca)
 
-embeded_ba = conver.converlutional(ph_behavior_asin_img) #out [-1,500] behavior img
+with tf.name_scope('embeded_ba'):
+    embeded_ba = conver.converlutional(ph_behavior_asin_img) #out [-1,500] behavior img
+    tf.summary.histogram('embeded_ba', embeded_ba)
+
 embeded_ba = tf.reshape(embeded_ba,[-1,500,1])
 
 img_init_state = cell.zero_state(batch_size=500,dtype = tf.float32) #batch size intented to be, out can be [-1, 1100] multiply oneby one
@@ -274,7 +285,9 @@ img_second_GRU_outputs, img_final_state_second = tf.nn.dynamic_rnn(cell, img_sec
 img_W_NN_input = weight_variable([500, 1])
 img_b_NN_input = bias_variable([1])
 img_NN_input_per= tf.nn.tanh(tf.matmul(tf.transpose(tf.reshape(img_second_GRU_outputs,[-1, 500])), tf.reshape(img_second_GRU_outputs,[-1, 500]))+img_b_NN_input)
-img_NN_input_per = tf.transpose(tf.nn.tanh(tf.matmul(img_NN_input_per, img_W_NN_input)+img_b_NN_input))
+with tf.name_scope('img_NN_input_per'):
+    img_NN_input_per = tf.transpose(tf.nn.tanh(tf.matmul(img_NN_input_per, img_W_NN_input)+img_b_NN_input))
+    tf.summary.histogram('img_NN_input_per', img_NN_input_per)
 # ————————————————————————————
 #img processing end
 # ————————————————————————————
@@ -287,17 +300,23 @@ img_NN_input_per = tf.transpose(tf.nn.tanh(tf.matmul(img_NN_input_per, img_W_NN_
 W_NN_input = weight_variable([1100, 1])
 b_NN_input = bias_variable([1])
 NN_input_per= tf.nn.tanh(tf.matmul(tf.transpose(tf.reshape(second_GRU_outputs,[-1, 1100])), tf.reshape(second_GRU_outputs,[-1, 1100]))+b_NN_input)
+
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@
 NN_input = tf.concat([tf.transpose(tf.nn.tanh(tf.matmul(NN_input_per, W_NN_input)+b_NN_input))
     ,ph_candidate_categories,ph_candidate_brand,ph_candidate_price,
     img_NN_input_per,embeded_ca], 1)
 
-W_fc_1 = weight_variable([6365, 200])
-b_fc_1 = bias_variable([200])
-h_fc_1 = tf.nn.tanh(tf.matmul(NN_input, W_fc_1)+b_fc_1)
+with tf.name_scope('h_fc_1'):
+    W_fc_1 = weight_variable([6365, 200])
+    b_fc_1 = bias_variable([200])
+    h_fc_1 = tf.nn.tanh(tf.matmul(NN_input, W_fc_1)+b_fc_1)
+    tf.summary.histogram('h_fc_1', h_fc_1)
 
-W_fc_2 = weight_variable([200, 80])
-b_fc_2 = bias_variable([80])
-h_fc_2 = tf.nn.tanh(tf.matmul(h_fc_1, W_fc_2)+b_fc_2)
+with tf.name_scope('h_fc_2'):
+    W_fc_2 = weight_variable([200, 80])
+    b_fc_2 = bias_variable([80])
+    h_fc_2 = tf.nn.tanh(tf.matmul(h_fc_1, W_fc_2)+b_fc_2)
+    tf.summary.histogram('h_fc_2', h_fc_2)
 
 W_fc_3 = weight_variable([80, 2])
 b_fc_3 = bias_variable([2])
@@ -305,7 +324,7 @@ final_result = tf.nn.softmax(tf.matmul(h_fc_2, W_fc_3)+b_fc_3)
 final_result = tf.nn.dropout(final_result, 0.5)
 loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=ph_label,logits=final_result))
 
-training_rate = 1e-4* (10**(-ph_epoch_num/10))
+training_rate = 1e-4* (10**(-ph_epoch_num/20))
 train_step = tf.train.AdamOptimizer(training_rate).minimize(loss)
 
 # train_step = tf.train.AdamOptimizer(1e-5).minimize(loss)
@@ -313,6 +332,9 @@ train_step = tf.train.AdamOptimizer(training_rate).minimize(loss)
 #NN end
 # ————————————————————————————
 with tf.Session() as sess:
+    merged = tf.summary.merge_all()
+    writer = tf.summary.FileWriter('/home/ubuntu/fyp2/Img_net/logs', sess.graph)
+    #writer = tf.summary.FileWriter('/logs', sess.graph)
     coord = tf.train.Coordinator()
     sess.run(tf.global_variables_initializer())
     sess.run(tf.local_variables_initializer())
@@ -339,7 +361,7 @@ with tf.Session() as sess:
             brt_val = np.array(brt_val).reshape((-1, 1))
             bp_val = np.array(bp_val).reshape((-1, 1))
 
-            sess.run(train_step, feed_dict=
+            _, loss_temp,re = sess.run([train_step, loss,merged], feed_dict=
             {ph_behavior_categories:bc_val, ph_behavior_brand:bb_val, 
             ph_behavior_review_time:brt_val,ph_behavior_price:bp_val,
             ph_candidate_categories:cc_val, ph_candidate_brand:cb_val, 
@@ -348,18 +370,10 @@ with tf.Session() as sess:
             ,ph_candidate_asign_img:ca_val
             ,ph_behavior_asin_img:ba_val})
 
-            if (global_step%500==0):
-                loss_temp= sess.run(loss, feed_dict=
-                {ph_behavior_categories:bc_val, ph_behavior_brand:bb_val, 
-                ph_behavior_review_time:brt_val,ph_behavior_price:bp_val,
-                ph_candidate_categories:cc_val, ph_candidate_brand:cb_val, 
-                ph_candidate_review_time:crt_val,ph_candidate_price:cp_val,
-                ph_label:l_val, ph_epoch_num:i
-                ,ph_candidate_asign_img:ca_val
-                ,ph_behavior_asin_img:ba_val})
-                epoch_loss = epoch_loss +loss_temp
-                five_k_loss = five_k_loss+loss_temp
-
+            epoch_loss = epoch_loss +loss_temp
+            five_k_loss = five_k_loss+loss_temp
+            if (global_step%2500==0):
+                writer.add_summary(re,global_step)
             if (global_step%5000==0):
                 current_rate= sess.run(training_rate, feed_dict=
                 {ph_behavior_categories:bc_val, ph_behavior_brand:bb_val, 
